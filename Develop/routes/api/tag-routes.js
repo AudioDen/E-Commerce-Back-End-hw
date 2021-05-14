30cd903f1390e9b5d5beef2c69010124701eb474
AudioDen <chicagosoundman@gmail.com>
2021-05-14 18:44:42 -0500

    begin routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -5,9 +5,22 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
-});
-
+  // be sure to include its associated Product data 
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+      model:Product,
+      attributes: ['id','product_name','price', 'stock','category_id' ]
+      }
+    ]
+})
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err =>{
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
@@ -15,14 +28,32 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
+// router.delete('/:id', (req, res) => {
+//   // delete on tag by its `id` value
+//   Tag.destroy({
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((deletedTag) => {
+//       res.json(deletedTag);
+//     })
+//     .catch((err) => res.json(err));
+// });
+
 
 module.exports = router;
